433e80b6feb2d901e28ca7e6e183a96295de5b67
Jessi251295 <jessicaqg25@gmail.com>
2026-04-28 15:41:20 -0500

    feat se crean las funciones registrar venta y ejecutar programa

--- Restaurante.py
+++ Restaurante.py
@@ -81,3 +81,72 @@ def eliminar_venta():
     except ValueError:
         print("Error: Ingrese un número válido.")
 
+def registrar_venta():
+    print("\n--- REGISTRAR NUEVA VENTA ---")    
+    if ventas_restaurante:
+        ids_actuales = [v["idVenta"] for v in ventas_restaurante]
+        nuevo_id = max(ids_actuales) + 1
+    else:
+        nuevo_id = 1
+    
+    nombre = input("Nombre del cliente: ")
+    mesa = int(input("Número de mesa: "))
+    plato = input("Plato principal: ")
+    valor = float(input("Valor del consumo: "))
+    
+  
+    print("\nSeleccione el método de pago:")
+    for i, metodo in enumerate(opciones_pago, 1):
+        print(f"{i}. {metodo}")
+    
+    while True:
+        try:
+            opc = int(input("Elija una opción (1-3): "))
+            if 1 <= opc <= len(opciones_pago):
+                pago_final = opciones_pago[opc - 1]
+                break
+            else:
+                print("Opción inválida.")
+        except ValueError:
+            print("Entrada inválida.")
+
+    nueva_v = {
+        "idVenta": nuevo_id,
+        "nombreCliente": nombre,
+        "numeroMesa": mesa,
+        "platoPrincipal": plato,
+        "valorConsumo": valor,
+        "metodoPago": pago_final,
+        "estadoPedido": "PENDIENTE"
+    }
+    ventas_restaurante.append(nueva_v)
+    print(f"¡Venta registrada con éxito! ID asignado: {nuevo_id} 🛒")
+
+def ejecutar_programa():
+    registrar_usuario()
+    if iniciar_sesion():
+        while True:
+            print("\n--- MENÚ DE GESTIÓN ---")
+            print("1. Ver todas las ventas")
+            print("2. Ordenar por valor (Menor a Mayor)")
+            print("3. Buscar venta por ID")
+            print("4. Eliminar venta")
+            print("5. Registrar nueva venta")
+            print("6. Salir")
+            
+            opcion = input("Seleccione una opción: ")
+            
+            if opcion == "1": mostrar_ventas()
+            elif opcion == "2": ordenar_ventas()
+            elif opcion == "3": buscar_venta()
+            elif opcion == "4": eliminar_venta()
+            elif opcion == "5": registrar_venta()
+            elif opcion == "6":
+                print("Cerrando sistema... ¡Hasta pronto! 👋")
+                break
+            else:
+                print("Opción no válida.")
+
+ejecutar_programa()
+
+
